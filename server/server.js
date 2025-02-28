07700ab84b6d1449e8b84c62dc70a5fecd7c0500
saduwka <nurzhan13071996@gmail.com>
2025-02-28 23:55:17 +0500

    Deploy task manager backend

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const WebSocket = require("ws");
 const admin = require("firebase-admin");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors());
